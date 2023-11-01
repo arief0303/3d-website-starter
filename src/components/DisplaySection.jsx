f2bfda3c6050d5b75f749e589173ed31aa0a5f8f
Arief R Syauqie <arief0303@gmail.com>
2023-11-01 18:02:48 +0700

    basis

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DisplaySection() {
+    return (
+        <div className="display-section wrapper">
+            <h2 className="title">Section 3</h2>
+            <p className="text">Lorem ipsum</p>
+            <span className="description">Lorem ipsum dolor sit amet.</span>
+            <button className="button">Try me!</button>
+            <button className="back-button">TOP</button>
+        </div>
+    );
+}
+
+export default DisplaySection;
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,16 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 function Jumbotron() {
+
+    const HandleLearnMore = () => {
+        const element = document.querySelector('.sound-section')
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
     return (
         <div className="jumbotron-section wrapper">
             <h2 className="title">Welcome to our store</h2>
@@ -18,7 +28,7 @@ function Jumbotron() {
                     <button className="button">Buy</button>
                 </li>
                 <li>
-                    <a className="link">Learn more</a>
+                    <a className="link" onClick={HandleLearnMore}>Learn more</a>
                 </li>
             </ul>
             {/* <img className='iphone-img' src={HoldingIphone} alt="holding iphone" /> */}
